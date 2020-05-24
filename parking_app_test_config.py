2e5add991f1507f61bea5a135eb891c9418bb2ec
HassanAzam <hazam@foree.co>
2020-05-25 04:08:48 +0500

    Added sample test config. | Added report of test results_v1

---
+++ parking_app_test_config.py
@@ -0,0 +1,8 @@
+SECRET_KEY = "blogger55"
+SQLALCHEMY_DATABASE_URI = "sqlite:///D:\\HassanWorkSpace\\flask-parking-app\\test_env_db.db"
+APP_ENV = "TESTING"
+PARKING_NUMBER_MAX_LENGTH = 6
+MAIL_SERVER = "64.233.184.108"
+MAIL_SERVER_PORT = 587
+DEFAULT_SENDER_EMAIL = "hassanazam55@gmail.com"
+DEFAULT_SENDER_EMAIL_PASSWORD = "facebook55"
